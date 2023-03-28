1d3eea9997c9b5e654ae80f01bb77d6549d88532
mohammad <aliakbariiimohammad@gmail.com>
2023-03-28 15:42:22 +0430

    complete function who wins

--- scripts.js
+++ scripts.js
@@ -26,4 +26,34 @@ function addGo(e){
     go = go === "circle" ? 'cross':'circle';
     infoDisplay.textContent = "its " +go+"'s turn"
     e.target.removeEventListener('click', addGo)
+    checkScore()
 }
+
+function checkScore(){
+    const allSquares = document.querySelectorAll(".square")
+    winning = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+    console.log()
+    winning.forEach((array) =>{
+        const circleWins = array.every(cell => allSquares[cell].firstChild ?. classList.contains('circle'))
+        if (circleWins){
+            infoDisplay.textContent = 'circle wins'
+    
+        }
+       
+    })
+
+    winning.forEach((array) =>{
+        const crossWins = array.every(cell => allSquares[cell].firstChild ?. classList.contains('cross'))
+        if (crossWins){
+            infoDisplay.textContent = 'cross wins'
+        }
+       
+    })
+        }
+    
+    
+
