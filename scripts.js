dd03222e3d020b7cffa5b7c76c1590773c9283af
mohammad <aliakbariiimohammad@gmail.com>
2023-03-28 14:10:44 +0430

    make circle shape in boardgame

--- scripts.js
+++ scripts.js
@@ -4,10 +4,19 @@ const startCells = [
     "","","","","","","","",""
 ]
 function createBoard(){
-    startCells.forEach(element => {
+    startCells.forEach((element,index) => {
         const cell = document.createElement('div')
         cell.classList.add('square')
+        cell.id = index
+        cell.addEventListener('click',addGo)
         gameBoard.append(cell)
     });
 }
 createBoard()
+
+function addGo(e){
+    const goDisplay = document.createElement('div')
+    goDisplay.classList.add("circle")
+    console.log(e.target)
+    e.target.append(goDisplay)
+}
